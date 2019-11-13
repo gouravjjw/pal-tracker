084c2ab95f42af18ded51ac43152674d8cad475d
gouravjjw <gourav.jjw2810@gmail.com>
2019-11-13 16:09:26 +0530

    Spring JDBC

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,11 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -11,8 +14,18 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
+    /*
     @Bean
     TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
+    }*/
+
+    @Bean
+    TimeEntryRepository timeEntryRepository() {
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
